3c22c8bc57fb073483728dbf07b8a0c933df1504
RahmaanM17 <ardrah17@gmail.com>
2023-11-06 21:45:38 +0700

    feat: update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
-        UpdateStatusTaskCommand::class
+        // UpdateStatusTaskCommand::class
     ];
     /**
      * Define the application's command schedule.
