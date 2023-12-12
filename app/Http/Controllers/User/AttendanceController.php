39bcebe72e6126ff4c47db7d509c199a13b35786
RahmaanM17 <ardrah17@gmail.com>
2023-12-12 07:27:19 +0700

    update: hour of attendance

--- app/Http/Controllers/User/AttendanceController.php
+++ app/Http/Controllers/User/AttendanceController.php
@@ -19,7 +19,7 @@ class AttendanceController extends Controller
     {
         if (session('auth.role') == "S") {
             if (
-                (intval(date('H', time() + 7 * 60 * 60)) == 8) ||
+                (intval(date('H', time() + 7 * 60 * 60)) == 7) ||
                 (intval(date('H', time() + 7 * 60 * 60)) == 16)
             ) {
                 $user = User::find(session('auth.id'));
