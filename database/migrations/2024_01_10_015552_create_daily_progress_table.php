cf5507d0fc4bbc276a56a83a1730e7a31bb7a263
RahmaanM17 <ardrah17@gmail.com>
2026-01-29 08:25:12 +0700

    feat: get to state development again after 3 years

--- database/migrations/2024_01_10_015552_create_daily_progress_table.php
+++ database/migrations/2024_01_10_015552_create_daily_progress_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->date('day');
             $table->string('evidence_file');
             $table->text('description');
-            $table->bigInteger('user_id');
+            $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')
                 ->references('id')
                 ->on('users')
--- resources/views/layouts/Authentication/index.blade.php
+++ resources/views/layouts/Authentication/index.blade.php
@@ -7,17 +7,17 @@
     <title>{{ env('APP_NAME') }}</title>
 
     <!-- General CSS Files -->
-    <link rel="stylesheet" href="{{ asset('modules/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('modules/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/modules/fontawesome/css/all.min.css') }}">
 
     <!-- CSS Libraries -->
-    <link rel="stylesheet" href="{{ asset('modules/jquery-selectric/selectric.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/modules/jquery-selectric/selectric.css') }}">
 
     <!-- Template CSS -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/components.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
     <!-- Start GA -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+    
     <script>
         window.dataLayer = window.dataLayer || [];
 
@@ -39,7 +39,7 @@
                     <div
                         class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                         <div class="login-brand">
-                            <img src="{{ asset('img/nug.png') }}" alt="logo" width="100"
+                            <img src="{{ asset('assets/img/nug.png') }}" alt="logo" width="100"
                                 class="shadow-light rounded-circle">
                         </div>
 
@@ -103,21 +103,21 @@
     </div>
 
     <!-- General JS Scripts -->
-    <script src="{{ asset('modules/jquery.min.js') }}"></script>
-    <script src="{{ asset('modules/popper.js') }}"></script>
-    <script src="{{ asset('modules/tooltip.js') }}"></script>
-    <script src="{{ asset('modules/bootstrap/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
-    <script src="{{ asset('modules/moment.min.js') }}"></script>
-    <script src="{{ asset('js/stisla.js') }}"></script>
+    <script src="{{ asset('assets/modules/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/modules/popper.js') }}"></script>
+    <script src="{{ asset('assets/modules/tooltip.js') }}"></script>
+    <script src="{{ asset('assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('assets/modules/moment.min.js') }}"></script>
+    <script src="{{ asset('assets/js/stisla.js') }}"></script>
 
     <!-- JS Libraies -->
 
     <!-- Page Specific JS File -->
 
     <!-- Template JS File -->
-    <script src="{{ asset('js/scripts.js') }}"></script>
-    <script src="{{ asset('js/custom.js') }}"></script>
+    <script src="{{ asset('assets/js/scripts.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
 </body>
 
 </html>
